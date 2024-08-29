5b854a62f8bdd95ef7c60f29c1e91215a49a12c4
Titikorn Tubsuk <titikorntarr@gmail.com>
2024-08-29 15:22:24 +0700

    first commit

---
+++ src/app/layout/frontlayout/frontlayout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frontlayout',
+  standalone: true,
+  imports: [],
+  templateUrl: './frontlayout.component.html',
+  styleUrl: './frontlayout.component.scss'
+})
+export class FrontlayoutComponent {
+
+}
